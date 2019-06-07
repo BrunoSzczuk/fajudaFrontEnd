9a8ca851d06b6656e384f31734196ea628e2e34a
patri <patricklucas524@gmail.com>
2019-06-07 20:59:53 -0300

    adicionado filter e paginator
    no cadastro de locais

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
 import { TipoAtendimento } from 'src/models/tipoatendimento';
 import { Atendimento } from 'src/models/atendimento';
 
-export var URL = "http://localhost:9090/";
+export var URL = "http://192.168.137.145:9090/";
 
 var FILTRO = "/filtro?";
 
